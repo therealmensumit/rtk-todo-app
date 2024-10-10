e4194d5e30aadb5570906d779c61c953506e4ca6
therealmensumit <sk03950@gmail.com>
2024-10-10 12:19:03 +0530

    "Update db.json, api.js, and vite.config.js files"

--- src/features/api.js
+++ src/features/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
   tagTypes: ["User"],
   endpoints: (builder) => ({
     getUsers: builder.query({
